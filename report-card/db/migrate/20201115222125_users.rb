fcedfb8637c5aef5f06755a1c8cc5cb9ca5d3667
Michael Iodice <miodice3@gmail.com>
2020-11-15 15:49:43 -0700

    setup userdb, user model, included macro has_secure_password and validating password encryption within tux

---
+++ report-card/db/migrate/20201115222125_users.rb
@@ -0,0 +1,25 @@
+class Users < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :username
+      t.string :password_digest
+
+      t.string :goal_enabled_1
+      t.string :goal_name_1
+      t.string :goal_frequency_1
+      t.string :goal_type_1
+      
+      t.string :goal_enabled_2
+      t.string :goal_name_2
+      t.string :goal_frequency_2
+      t.string :goal_type_2
+
+      t.string :goal_enabled_3
+      t.string :goal_name_3
+      t.string :goal_frequency_3
+      t.string :goal_type_3
+    end
+  end
+end
