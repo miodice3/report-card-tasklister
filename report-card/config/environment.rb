1118d0c97e29157ba33085460792dfd7bc61c3fe
Michael Iodice <miodice3@gmail.com>
2020-11-15 15:07:13 -0700

    First test submit, set up application with corneal and removed testing gems to avoid errors.

---
+++ report-card/config/environment.rb
@@ -0,0 +1,19 @@
+ENV['SINATRA_ENV'] ||= "development"
+
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
+
+require './app/controllers/application_controller'
+require_all 'app'
+
+
+=begin
+git add .
+git commit -m " Put your notes here! "     
+git push   <then check github
+=end
