a5786047952ffdf742b2f9b6e0081387833b8301
Michael Iodice <miodice3@gmail.com>
2020-11-18 17:52:35 -0700

    basic linking between all dates, user profile & goals completed.  clicking into dates now lists all dates belonging to a user and next i will link these to find or create new record for join table instances, to keep track of goaldatecards uniquely associated by goal and date belonging to the users.

--- report-card/app/controllers/goals_controller.rb
+++ report-card/app/controllers/goals_controller.rb
@@ -7,33 +7,33 @@ class GoalsController < ApplicationController
     end
 
     get '/goals' do
-        binding.pry
+        #binding.pry
         @goals = Goal.all
         #binding.pry
         erb :'goals/index'
     end
 
     get '/goals/new' do
-        binding.pry
+        #binding.pry
         erb :'goals/new'
     end
 
     post '/goals/mygoals' do
-        binding.pry
+        #binding.pry
         @user=User.all.find_by_id(session[:user_id])
         @goals = @user.goals
         erb :'goals/mygoals'
     end
 
     get '/goals/mygoals' do
-        binding.pry
+        #binding.pry
         @user=User.all.find_by_id(session[:user_id])
         @goals = @user.goals
         erb :'goals/mygoals'
     end
 
     post '/goals' do
-        binding.pry
+        #binding.pry
         #binding.pry
         @goal = Goal.create(params[:goal])
         @goal.user_id=session[:user_id]
@@ -41,25 +41,25 @@ class GoalsController < ApplicationController
         redirect "/goals/#{@goal.id}"
     end
 
-
-
-    get '/goals/:id/edit' do
-        binding.pry
-        @user = User.find_by_id(params[:id])
-        @goal = Goal.find_by_id(params[:id])
-        erb :'goals/edit'
-    end
-
     get '/goals/:id' do
-        binding.pry
+        #binding.pry
         @goal = Goal.find_by_id(params[:id])
         @user = User.find_by_id(session[:user_id])
         #binding.pry
         erb :'goals/show'
     end
 
+    get '/goals/:id/edit' do
+        #binding.pry
+        @user=User.find_by_id(session[:user_id])
+        @goal = Goal.find_by_id(params[:id])
+        erb :'goals/edit'
+    end
+
+
+
     patch '/goals/:id' do
-        binding.pry
+        #binding.pry
         #binding.pry
         #currently any user can edit any goal
         goal = Goal.find_by_id(params[:id])
@@ -69,10 +69,10 @@ class GoalsController < ApplicationController
     end
 
     delete '/goals/:id' do
-        binding.pry
+        #binding.pry
         goal = Goal.find_by_id(params[:id])
         goal.destroy
-        redirect "/goals"
+        redirect "/goals/mygoals"
     end
 
 end
