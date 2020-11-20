8c50d75c08d37ffc143bb64178af0acf5fbb2c10
Michael Iodice <miodice3@gmail.com>
2020-11-20 09:43:26 -0700

    updated individual date / goal display page to hide information depending on goal type so the user does not have to analyze form and make a decision / mistake. this should reduce the number of errors via manual mistake error getting into the db and ensure accurate records.  still would like to make a validation per date, something color coded, so the user can easily see if certain dates or date goal cards need updating without having to guess.

--- report-card/config/environment.rb
+++ report-card/config/environment.rb
@@ -17,15 +17,4 @@ require_all 'app'
 git add .
 git commit -m " Put your notes here! "     
 git push   <then check github
-
-
-To talk in Office Hours:
-1A - problem with edit form and checking boxes depending on state of varialbe
-1B - how to increment the variable state during the loop for the edit phase? this will save hard coding the forms and allow flexible goals
-1B can be solved by having a separate class handle the assembly of parameters, depending on state of report card and goals.
-
-to edit:
-can change array size to pickup from db column row and not have the @range things all over the place
-
-
 =end
